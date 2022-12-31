a69dc65f3207ffc3e5d6aea66b3700715b999033
Gor <7495500@gmail.com>
2022-12-31 19:33:25 +0300

    Change database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_31_124917) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_25_171703) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,7 +81,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_31_124917) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "guide_code_id"
     t.index ["name"], name: "index_langs_on_name"
   end
 
